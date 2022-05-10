fe3ab1d703b907172cd445bb2e0530110b2a2321
Ronaldo Chelone <ronaldo_chelone@yahoo.com.br>
2022-05-10 17:42:51 -0300

    Atualizaçã do Cabecalho para metodos PUT e DELETE

--- public_html/index.php
+++ public_html/index.php
@@ -2,17 +2,19 @@
 
 namespace App;
 
-// Evitar cros no apache2 //
-$method_xsff = $_SERVER['REQUEST_METHOD'];
-if ($method_xsff == "OPTIONS") {
-    header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-    die();
-} else {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Headers: *');
-    header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE');
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
+}
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    }
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    }
+    exit(0);
 }
 
 
@@ -39,16 +41,16 @@ if ($url) {
         array_shift($url);
 
         $params = $url;
-
         switch ($_SERVER["REQUEST_METHOD"]) {
             case "GET":
                 $method = "GET";
                 break;
             case "POST":
-                 $method = "POST";
+                $method = "POST";
                 break;
             case "PUT":
                 $method = "PUT";
+                parse_str(file_get_contents("php://input"), $_POST);
                 break;
             case "DELETE":
                 $method = "DELETE";
