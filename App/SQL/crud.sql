6204c9f436549d441671614e4b72751c90d987a7
Ronaldo Chelone <ronaldo_chelone@yahoo.com.br>
2022-05-11 02:15:33 -0300

    Atualização do Banco de Dados

--- App/SQL/crud.sql
+++ App/SQL/crud.sql
@@ -1,8 +1,8 @@
 -- --------------------------------------------------------
--- Servidor:                     localhost
--- Versão do servidor:           10.2.3-MariaDB-log - mariadb.org binary distribution
--- OS do Servidor:               Win32
--- HeidiSQL Versão:              9.4.0.5125
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           5.7.33 - MySQL Community Server (GPL)
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              11.2.0.6213
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -10,12 +10,33 @@
 /*!50503 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
 -- Copiando estrutura do banco de dados para crud
 DROP DATABASE IF EXISTS `crud`;
 CREATE DATABASE IF NOT EXISTS `crud` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `crud`;
+
+-- Copiando estrutura para tabela crud.address
+DROP TABLE IF EXISTS `address`;
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `logradouro` varchar(100) DEFAULT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `cidade` varchar(100) DEFAULT NULL,
+  `estado` varchar(2) DEFAULT NULL,
+  `cep` varchar(20) DEFAULT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Copiando dados para a tabela crud.address: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `address` DISABLE KEYS */;
+INSERT INTO `address` (`id`, `logradouro`, `bairro`, `cidade`, `estado`, `cep`, `created_at`, `updated_at`) VALUES
+	(1, 'Praça Antônio Elias Moisés', 'Centro', 'Belo Horizonte', 'MG', '30110000', '2022-05-11 02:13:25', '2022-05-11 02:13:25');
+/*!40000 ALTER TABLE `address` ENABLE KEYS */;
 
 -- Copiando estrutura para tabela crud.user
 DROP TABLE IF EXISTS `user`;
@@ -27,12 +48,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `cpf` varchar(20) NOT NULL DEFAULT '0',
   `telefone` varchar(20) NOT NULL DEFAULT '0',
   `cep` varchar(20) NOT NULL DEFAULT '0',
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela crud.user: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `nome`, `email`, `data_nascimento`, `cpf`, `telefone`, `cep`, `created_at`, `updated_at`) VALUES
+	(1, 'fULANO', 'fulano@domain', '2000-01-01', '231315', '1111111', '30110000', '2022-05-11 02:13:25', '2022-05-11 02:13:25');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
